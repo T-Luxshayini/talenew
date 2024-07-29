e716ade566a02c72ff97b50106b3687344da4431
T-Luxshayini <luxshayini1203@gmail.com>
2024-07-30 04:55:39 +0530

    Updates

---
+++ precache-manifest.641f930d7e2549b05a77fa94458f2c18.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d67d0916d8a5397fb66028589ec850",
+    "url": "/talenew/index.html"
+  },
+  {
+    "revision": "17e6da2f4ad188e9b636",
+    "url": "/talenew/static/css/main.0d352a0f.chunk.css"
+  },
+  {
+    "revision": "06f86e6a8c59fcc5228f",
+    "url": "/talenew/static/js/2.dcddaa47.chunk.js"
+  },
+  {
+    "revision": "60c46b200b0cfd41617f",
+    "url": "/talenew/static/js/3.b265e9c7.chunk.js"
+  },
+  {
+    "revision": "17e6da2f4ad188e9b636",
+    "url": "/talenew/static/js/main.f075b613.chunk.js"
+  },
+  {
+    "revision": "d8c79c5016e884361938",
+    "url": "/talenew/static/js/runtime~main.87ae3208.js"
+  },
+  {
+    "revision": "d4be36233795e6a2037824b876797079",
+    "url": "/talenew/static/media/about-us-image.d4be3623.jpg"
+  },
+  {
+    "revision": "1d76e19e9eae608b7c846d6da276ab6e",
+    "url": "/talenew/static/media/banner-right.1d76e19e.png"
+  },
+  {
+    "revision": "02584ab4f68c565465fca3dbd3182c08",
+    "url": "/talenew/static/media/cta-bg.02584ab4.jpg"
+  },
+  {
+    "revision": "ee91e640b5449fb98d9320c877a9866e",
+    "url": "/talenew/static/media/fa-brands-400.ee91e640.woff2"
+  },
+  {
+    "revision": "f34b6a2a94e1a01e4c21fa84dcbf6667",
+    "url": "/talenew/static/media/fa-brands-400.f34b6a2a.ttf"
+  },
+  {
+    "revision": "65e80529f5cfcf16a4b1161601a1616c",
+    "url": "/talenew/static/media/fa-regular-400.65e80529.ttf"
+  },
+  {
+    "revision": "82bafee9dcc7b6fb7bca7ed323f9b7ae",
+    "url": "/talenew/static/media/fa-regular-400.82bafee9.woff2"
+  },
+  {
+    "revision": "52afeb7a328694838c6b073ad7af24d8",
+    "url": "/talenew/static/media/fa-solid-900.52afeb7a.ttf"
+  },
+  {
+    "revision": "57b380d27f14f16e737bcca7e849cf79",
+    "url": "/talenew/static/media/fa-solid-900.57b380d2.woff2"
+  },
+  {
+    "revision": "43044320c62b2b1397b8a0d535dea6a7",
+    "url": "/talenew/static/media/fa-v4compatibility.43044320.woff2"
+  },
+  {
+    "revision": "9d6f359a328ee3df73709c1d8f05b0d4",
+    "url": "/talenew/static/media/fa-v4compatibility.9d6f359a.ttf"
+  },
+  {
+    "revision": "9bf16f227dee28539de435eee8baee8e",
+    "url": "/talenew/static/media/faqs-image.9bf16f22.jpg"
+  },
+  {
+    "revision": "5519863b7ba8eb7e4eb9e27a298b30a5",
+    "url": "/talenew/static/media/happyclient-01.5519863b.jpg"
+  },
+  {
+    "revision": "b2dcc2adf4c02903fa8de45cb8fd48a7",
+    "url": "/talenew/static/media/infos-bg.b2dcc2ad.jpg"
+  },
+  {
+    "revision": "751ad33bab15a798c4f2d8e2d4820dc8",
+    "url": "/talenew/static/media/left-infos.751ad33b.jpg"
+  },
+  {
+    "revision": "b346925809d213f0c23cbc9d8f595fe0",
+    "url": "/talenew/static/media/services-left.b3469258.jpg"
+  },
+  {
+    "revision": "de7b272946c6c07fa1784ac248c882a1",
+    "url": "/talenew/static/media/video-bg.de7b2729.jpg"
+  },
+  {
+    "revision": "72baace433fbca7b0f64385557fe0df7",
+    "url": "/talenew/static/media/video-thumb.72baace4.jpg"
+  }
+]);
